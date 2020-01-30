93ee9f3935bf8b171e45d55a7d65852b6b2fe24a
Belarus <tsevanvadim25@gmail.com>
2020-01-30 13:11:00 +0200

    Выполнил второе задание ко второму уроку.

---
+++ src/jc01_2020.Tsevan25/jc01_2020/lesson02/Task2.java
@@ -0,0 +1,35 @@
+package jc01_2020.Tsevan25.jc01_2020.lesson02;
+
+/*
+*
+* Делится ли введенное число на 3 без остатка
+* С клавиатуры вводится число. Вывести на экран делится ли оно на 3 без остатка
+*
+* Формат вывода (ошибки недопустимы):
+* "Делится"
+* "Не делится"
+*
+*/
+
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите целое число");
+        long numeric = scanner.nextLong();
+        if (numeric % 3 == 0) {
+            System.out.println("Делится");
+        } else {
+            System.out.println("Не делится");
+        }
+
+        long numeric2 = scanner.nextLong();
+        if (numeric2 % 3 == 0) {
+            System.out.println("Делится");
+        } else {
+            System.out.println("Не делится");
+        }
+
+    }
+}
