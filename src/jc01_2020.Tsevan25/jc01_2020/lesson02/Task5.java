5b172f9de13d903a1056dc71c71e2a484c526358
Tsevan25 <60292426+Tsevan25@users.noreply.github.com>
2020-02-02 19:43:11 +0200

    Update Task5.java

--- src/jc01_2020.Tsevan25/jc01_2020/lesson02/Task5.java
+++ src/jc01_2020.Tsevan25/jc01_2020/lesson02/Task5.java
@@ -19,5 +19,11 @@ public class Task5 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int year = scanner.nextInt();
+		   int year = scanner.nextInt();
+            	if(year%4==0 && year%100 !=0 || year%400==0){
+                System.out.println("Високосный");
+           	 } else {
+                System.out.println("Обычный");
+            }
 	}
 }
