13ba1753e964e72be438588169a32e0c10ee13f5
Belarus <tsevanvadim25@gmail.com>
2020-02-06 14:03:37 +0200

    Урок 3. Задание 1.

--- src/jc01_2020.Tsevan25/jc01_2020/lesson03/Task1.java
+++ src/jc01_2020.Tsevan25/jc01_2020/lesson03/Task1.java
@@ -14,6 +14,20 @@ import java.util.Scanner;
 public class Task1 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		System.out.println("Введите натуральное число");
 		long input = scanner.nextLong();
+		long maxNumber, minNumber;
+		maxNumber = minNumber = (byte)(input % 10);
+		while (input > 0) {
+			long x = (byte)(input % 10);
+			if (x > maxNumber)
+				maxNumber = x;
+			if (x < minNumber)
+				minNumber = x;
+			input /= 10;
+		}
+		System.out.println(maxNumber);
+
+
 	}
 }
