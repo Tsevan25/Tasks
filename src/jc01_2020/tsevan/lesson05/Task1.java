69fab8f9af81fcf65a7b8c5364c91e5f12c3625a
Tsevan25 <60292426+Tsevan25@users.noreply.github.com>
2020-02-23 17:13:54 +0200

    Update Task1.java

--- src/jc01_2020/tsevan/lesson05/Task1.java
+++ src/jc01_2020/tsevan/lesson05/Task1.java
@@ -16,7 +16,15 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		String str = scanner.nextLine();
+	Scanner scanner = new Scanner(System.in);
+	String str = scanner.nextLine();
+        String reg = "^[a-zA-Z_]{1}[a-zA-Z0-9_]+";
+        Pattern pattern = Pattern.compile(reg);
+        Matcher matcher = pattern.matcher(str);
+        if (Pattern.matches("^[a-zA-Z_]{1}[a-zA-Z0-9_]+", str)){
+            System.out.println("Да");
+        } else {
+            System.out.println("Нет");
+        }
     }
 }
