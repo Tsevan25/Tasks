441b5e93a9864c99d6a56e22c6e5aa697d6c85ff
Tsevan25 <60292426+Tsevan25@users.noreply.github.com>
2020-02-23 17:14:50 +0200

    Update Task2.java

--- src/jc01_2020/tsevan/lesson05/Task2.java
+++ src/jc01_2020/tsevan/lesson05/Task2.java
@@ -17,6 +17,22 @@ import java.util.Scanner;
 public class Task2 {
     public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		String str = scanner.nextLine();
+        String str = scanner.nextLine();
+        boolean changeling = true;
+        String NoSpaceText = str.replaceAll(" ", "").toLowerCase();
+        int share = NoSpaceText.length() / 2;
+        for (int i = 0; i < share; i++) {
+            if (NoSpaceText.charAt(i) != NoSpaceText.charAt(NoSpaceText.length() - i - 1)) {
+                changeling = false;
+                break;
+            }
+        }
+        if (changeling) {
+            System.out.println("Да");
+        } else {
+            System.out.println("Нет");
+        }
+        System.out.println(NoSpaceText);
     }
 }
+
