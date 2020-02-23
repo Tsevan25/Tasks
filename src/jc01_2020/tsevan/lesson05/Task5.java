8fb6c492198a99590947d447734721b1cebf294e
Tsevan25 <60292426+Tsevan25@users.noreply.github.com>
2020-02-23 17:17:31 +0200

    Update Task5.java

--- src/jc01_2020/tsevan/lesson05/Task5.java
+++ src/jc01_2020/tsevan/lesson05/Task5.java
@@ -14,7 +14,20 @@ import java.util.Scanner;
 
 public class Task5 {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		String str = scanner.nextLine();
-	}
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        str = str.replace(" ", "");
+
+        StringBuilder string = new StringBuilder(str);
+        for (int i = 0; i < string.length(); i++) {
+            char SymbolOne = string.charAt(i);
+            for (int j = string.length() - 1; j > i; j--) {
+                char SymbolTwo = string.charAt(j);
+                if (SymbolOne == SymbolTwo) {
+                    string.deleteCharAt(j);
+                }
+            }
+        }
+        System.out.println(string);
+    }
 }
