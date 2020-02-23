f8bc403b7a1ec142f8b3ca7ff0c791049ca6a585
Tsevan25 <60292426+Tsevan25@users.noreply.github.com>
2020-02-23 17:06:54 +0200

    Update Task5.java

--- src/jc01_2020/tsevan/lesson04/Task5.java
+++ src/jc01_2020/tsevan/lesson04/Task5.java
@@ -16,16 +16,19 @@ import java.util.Scanner;
 
 public class Task5 {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int[][] array = new int[4][3];
-		int max = 0;
-		for (int i = 0; i < array[0].length; i++) {
-			for (int j = 0; j < array.length; j++) {
-				array[j][i] = scanner.nextInt();
-				if (i > 0) {
-				array[j][i] += array[j][i];
-				max = array[j][i] > array[max][i] ? j : max;
-				} } }
-		System.out.println("Максимальную сумму содержит столбец под индексом: " + max);
-	} }
+        Scanner scanner = new Scanner(System.in);
+        int[][] array = new int[4][3];
+        int max = 0;
+        for (int i = 0; i < array[0].length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                array[j][i] = scanner.nextInt();
+                if (i > 0) {
+                    array[j][i] += array[j][i];
+                    max = array[j][i] > array[max][i] ? j : max;
+                }
+            }
+        }
+        System.out.println("Максимальную сумму содержит столбец под индексом: " + max);
+    }
+}
 
