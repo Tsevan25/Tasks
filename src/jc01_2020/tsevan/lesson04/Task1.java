3aad2e727d7867065072b0e82aa063be6230789c
Tsevan25 <60292426+Tsevan25@users.noreply.github.com>
2020-02-23 17:01:34 +0200

    Update Task1.java

--- src/jc01_2020/tsevan/lesson04/Task1.java
+++ src/jc01_2020/tsevan/lesson04/Task1.java
@@ -16,13 +16,15 @@ public class Task1 {
     public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int[] array = new int[7];
-		int sum =0;
-		for (int i = 0; i < array.length; i++) {
-			array[i] = scanner.nextInt();
+		int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
 
-			if ((array[i]%2 == 0) && (array[i]>0))
-			{ sum = array[i] + sum;
-			}  }
-		System.out.println(sum);
-    } }
+            if ((array[i] % 2 == 0) && (array[i] > 0)) {
+                sum = array[i] + sum;
+            }
+        }
+        System.out.println(sum);
+    }
+}
 
