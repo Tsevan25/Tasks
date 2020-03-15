c103c629c5235de12d0596fe1e6617b499e4bc2d
Belarus <tsevanvadim25@gmail.com>
2020-03-15 13:03:17 +0200

    Update

---
+++ src/jc01_2020/tsevan/test01/Comparision.java
@@ -0,0 +1,24 @@
+package jc01_2020.tsevan.test01;
+
+/*
+ *
+ * Считать из консоли строку, состоящую из цифр.
+ * Вычислить firstSum как сумму каждого четвертого элемента. В сумму включать только те цифры, которые больше 5
+ * Вычислить secondSum как сумму каждого второго элемента. В сумму включать только те цифры, которые меньше 5
+ * Вывести в консоль большее из значений firstSum и secondSum или слово "равны", если они совпадают
+ *
+ */
+
+import java.util.Scanner;
+
+public class Comparision {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
+		int firstSum = 0;
+		int secondSum = 0;
+		int[] array = new int[input.length()];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = scanner.nextInt();
+
+}}}
