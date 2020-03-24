54701487c26400d615ba7bd3059c4126ab6c8990
Belarus <tsevanvadim25@gmail.com>
2020-03-24 19:04:07 +0200

    Update

--- src/jc01_2020/tsevan/lesson13/Task3.java
+++ src/jc01_2020/tsevan/lesson13/Task3.java
@@ -10,8 +10,21 @@ package jc01_2020.tsevan.lesson13;
  *
  */
 
-public class Task3 {
-	public static void main(String[] args) {
 
-	}
+import jdk.nashorn.api.scripting.ScriptObjectMirror;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task3 {
+    public static void main(String[] args) {
+		Map<String, LocalDate> map = new HashMap<>();
+		map.put("Александр", LocalDate.of(1990, 4, 10));
+		map.put("Михаил", LocalDate.of(1980, 5, 20));
+		map.put("Екатерина", LocalDate.of(2000, 6, 30));
+		map.put("Лилия", LocalDate.of(2010, 7, 1));
+		System.out.println(map);
+    
+		}
 }
